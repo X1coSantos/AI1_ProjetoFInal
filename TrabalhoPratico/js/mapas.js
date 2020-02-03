78f7831e0d426eae9a0e462fe342342cc77f3892
Francisco Santos <franciscoslb.98@hotmail.com>
2020-02-03 10:23:25 +0000

    cenas

--- TrabalhoPratico/js/mapas.js
+++ TrabalhoPratico/js/mapas.js
@@ -4,9 +4,15 @@ var modalMapas = document.getElementById("modalMapas");
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var imgMapas = document.getElementsByClassName("thumb");
 
+
+var rotasTitulo = document.getElementById("rotasInfo-titulo");
+var rotasImg = document.getElementById("rotasPainelEsquerdoIMG");
+
 for ( var i = 0; i < imgMapas.length; i++ ) (function(i){ 
 	imgMapas[i].onclick = function() {
 		modalMapas.style.display = "block";
+		rotasTitulo.textContent = "Titulo de teste";
+		rotasImg.src = imgMapas[i].children[0].children[0].src
 	}
   })(i);
 
